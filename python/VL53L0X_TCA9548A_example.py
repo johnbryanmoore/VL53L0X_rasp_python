53b871746f947cdc501747f0bfed0cfd9e61292d
Ramsay, Grant (NZ) <grant.ramsay@cubic.com>
2018-01-11 15:55:16 +1300

    Update examples to interface change

--- python/VL53L0X_TCA9548A_example.py
+++ python/VL53L0X_TCA9548A_example.py
@@ -26,33 +26,37 @@ import time
 import VL53L0X
 
 # Create a VL53L0X object for device on TCA9548A bus 1
-tof1 = VL53L0X.VL53L0X(TCA9548A_Num=1, TCA9548A_Addr=0x70)
+tof1 = VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
 # Create a VL53L0X object for device on TCA9548A bus 2
-tof2 = VL53L0X.VL53L0X(TCA9548A_Num=2, TCA9548A_Addr=0x70)
+tof2 = VL53L0X.VL53L0X(tca9548a_num=2, tca9548a_addr=0x70)
+tof1.open()
+tof2.open()
 
 # Start ranging on TCA9548A bus 1
-tof1.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+tof1.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 # Start ranging on TCA9548A bus 2
-tof2.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+tof2.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
 timing = tof1.get_timing()
-if (timing < 20000):
+if timing < 20000:
     timing = 20000
-print ("Timing %d ms" % (timing/1000))
+print("Timing %d ms" % (timing/1000))
 
-for count in range(1,101):
+for count in range(1, 101):
     # Get distance from VL53L0X  on TCA9548A bus 1
     distance = tof1.get_distance()
-    if (distance > 0):
-        print ("1: %d mm, %d cm, %d" % (distance, (distance/10), count))
+    if distance > 0:
+        print("1: %d mm, %d cm, %d" % (distance, (distance/10), count))
 
     # Get distance from VL53L0X  on TCA9548A bus 2
     distance = tof2.get_distance()
-    if (distance > 0):
-        print ("2: %d mm, %d cm, %d" % (distance, (distance/10), count))
+    if distance > 0:
+        print("2: %d mm, %d cm, %d" % (distance, (distance/10), count))
 
     time.sleep(timing/1000000.00)
 
 tof1.stop_ranging()
 tof2.stop_ranging()
 
+tof1.close()
+tof2.close()
